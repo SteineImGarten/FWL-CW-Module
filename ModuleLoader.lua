6f4ea3b7be9288a0f03d08af9cceb60e09880f4d
SteineImGarten <hausslermichael414@gmail.com>
2025-10-27 00:27:48 +0100

    Update ModuleLoader.lua

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -1,4 +1,5 @@
 local Logger
+
 pcall(function()
     local Console = require(game.ReplicatedStorage.Packages.Console)
     Logger = Console.New("HookLoader")
@@ -8,6 +9,7 @@ local Loader = {}
 
 local Folders = {}
 local Debug = false
+local GlobalTable = getgenv()
 
 Loader.Debug = function(State)
     Debug = State and true or false
@@ -15,6 +17,12 @@ end
 
 Loader.Folders = function(List)
     Folders = List or {}
+end
+
+Loader.Global = function(Table)
+    if type(Table) == "table" then
+        GlobalTable = Table
+    end
 end
 
 local function Safe(Module)
@@ -42,13 +50,18 @@ Loader.Load = function()
             if Module:IsA("ModuleScript") then
                 local Tbl = Safe(Module)
                 if Tbl then
-                    Mods["@"..Module.Name] = Tbl
+                    local Key = "@" .. Module.Name
+                    Mods[Key] = Tbl
                     if Debug then
                         print(("Load module: %s"):format(Module:GetFullName()))
                     end
                 end
             end
         end
+    end
+
+    for Key, Val in pairs(Mods) do
+        GlobalTable[Key] = Val
     end
 
     if Debug then
@@ -60,4 +73,52 @@ Loader.Load = function()
     return Mods
 end
 
+Loader.Call = function(ModuleKey, FunctionName, ...)
+    local Mod = GlobalTable[ModuleKey]
+    if not Mod then
+        warn(("Module %s not found"):format(ModuleKey))
+        return nil
+    end
+
+    local Func = Mod[FunctionName]
+    if typeof(Func) ~= "function" then
+        warn(("Function %s not found in module %s"):format(FunctionName, ModuleKey))
+        return nil
+    end
+
+    return Func(...)
+end
+
+Loader.Hook = function(ModuleKey, FunctionName, HookFunc)
+    local Mod = GlobalTable[ModuleKey]
+    if not Mod then
+        warn(("Module %s not found"):format(ModuleKey))
+        return nil
+    end
+
+    local OrigFunc = Mod[FunctionName]
+    if typeof(OrigFunc) ~= "function" then
+        warn(("Function %s not found in module %s"):format(FunctionName, ModuleKey))
+        return nil
+    end
+
+    local Success, Hooked = pcall(function()
+        return hookfunction(OrigFunc, HookFunc)
+    end)
+
+    if not Success then
+        warn(("Failed to hook %s in module %s: %s"):format(FunctionName, ModuleKey, tostring(Hooked)))
+        return nil
+    end
+
+    if Debug then
+        print(("Hooked %s in module %s"):format(FunctionName, ModuleKey))
+    end
+
+    return Hooked
+end
+
+GlobalTable.HookLoader = Loader
+
 return Loader
+
