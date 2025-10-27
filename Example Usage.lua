a9d42129a0981cd58c5611c34ce61c89a5e291a4
SteineImGarten <hausslermichael414@gmail.com>
2025-10-27 21:08:48 +0100

    Update Example Usage.lua

--- Example Usage.lua
+++ Example Usage.lua
@@ -1,7 +1,16 @@
 local HL = loadstring(game:HttpGet("https://raw.githubusercontent.com/SteineImGarten/FWL-CW-Module/refs/heads/main/ModuleLoader.lua"))()
 
+----------------------------------------------------------------------------
+
+            -- Settings --
+
 HL.Debug(false)
 HL.Global(getgenv())
+
+----------------------------------------------------------------------------
+
+            -- Load Modules --
+
 HL.Folders({
     game.ReplicatedStorage.Client.Source,
     game.ReplicatedStorage.Shared.Source,
@@ -10,18 +19,70 @@ HL.Folders({
 
 HL.Load()
 
-----------------------------------
 
---Call a function
+----------------------------------------------------------------------------
 
-HL.Call("@ToastNotificationActionsClient", "add", "success", "Ah!", 5, true, {BypassHook = true})(getgenv()["@RoduxStore"].store)
+            -- Call Game Function
+
+HL.Call("@ToastNotificationActionsClient", "add", "success", "Hook Finished", 5, true, {BypassHook = false})(getgenv()["@RoduxStore"].store)
+HL.Call("@SoundHandler", "playSound", {
+    soundObject = game:GetService("ReplicatedStorage").Shared.Assets.Sounds.Success2,
+    parent = Workspace.Sounds
+})
 
 
--------------------------------------------
+----------------------------------------------------------------------------
 
--- Hook a function
 
-HL.Hook("@ToastNotificationActionsClient", "add", function(Original, Type, Text, Duration, ShouldToast)
-    Text = "Hooked"
+            -- Example hook --
+
+HL.Hook("@ToastNotificationActionsClient", "add", "ConfigOne", function(Original, Type, Text, Duration, ShouldToast)
+        print(Type)
+        print(Text)
     return Original(Type, Text, Duration, ShouldToast)
 end)
+
+
+----------------------------------------------------------------------------
+
+            -- Example --
+
+--HL.ViewHookIDs("@ExampleModule", "FunctionName")
+
+--HL.Unhook("@ExampleModule", "FunctionName")
+
+--HL.Call("@ExampleModule", "FunctionName")
+
+----------------------------------------------------------------------------
+
+
+            -- Example: search function --
+
+HL.ShowFunc("spendStamina")
+
+
+----------------------------------------------------------------------------
+
+            -- Example: modifications  --
+
+getgenv()["@JumpConstants"]["JUMP_DELAY_ADD"] = 0
+
+HL.Hook("@DefaultStaminaHandlerClient", "spendStamina", "ConfigMaxed", function(Original, Stamina)
+        Stamina = 1
+    return Original(Stamina)
+end)
+
+
+----------------------------------------------------------------------------
+
+            -- Stamina Mods --
+
+local DefaultStamina = HL.Call("@DefaultStaminaHandlerClient", "getDefaultStamina")
+
+HL.Call("@Stamina", "setBaseMaxStamina", DefaultStamina, 200)
+HL.Call("@Stamina", "setStamina", DefaultStamina, 1)
+
+DefaultStamina.gainDelay = 0.1
+DefaultStamina.gainPerSecond = 150
+
+----------------------------------------------------------------------------
